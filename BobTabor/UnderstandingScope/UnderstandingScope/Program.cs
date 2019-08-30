7643cd783d40a5e788b273d2f77a107b6fa51031
doomedcow <Gymlet@users.noreply.github.com>
2019-08-30 11:22:49 -0700

    Initial Commit

---
+++ BobTabor/UnderstandingScope/UnderstandingScope/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UnderstandingScope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
