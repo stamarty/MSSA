7ed8ae6b49ea969534b1372d072273638529a4cd
doomedcow <Gymlet@users.noreply.github.com>
2019-08-30 11:26:46 -0700

    Now it counts to 10.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,13 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
         }
     }
 }
