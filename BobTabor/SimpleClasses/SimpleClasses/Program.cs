f0b5898770582c0ab5eb4c567ddfb6e514d1d235
doomedcow <Gymlet@users.noreply.github.com>
2019-08-30 11:13:06 -0700

    Complete Program.

--- BobTabor/SimpleClasses/SimpleClasses/Program.cs
+++ BobTabor/SimpleClasses/SimpleClasses/Program.cs
@@ -6,7 +6,48 @@ namespace SimpleClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Tundra";
+            myCar.Year = 2012;
+            myCar.Color = "Black";
+
+            Console.WriteLine("{0} {1} {2} {3}", 
+                myCar.Make, 
+                myCar.Model, 
+                myCar.Year, 
+                myCar.Color);
+
+            //decimal value = DetermineMarketValue(myCar);
+            //Console.WriteLine("This car is worth {0:C}", value);
+
+            Console.WriteLine("This car is worth {0:C}", myCar.DetermineMarketValue());
+
+            Console.ReadLine();
+        }
+
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 100.0M;
+            return carValue;
+        }
+    }
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+            if (Year > 1990)
+                carValue = 10000;
+            else
+                carValue = 2000;
+            return carValue;
         }
     }
 }
+
