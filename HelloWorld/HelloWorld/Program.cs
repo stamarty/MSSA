fcb41d106209a64385ce4ccd4a65256de66a6c26
doomedcow <Gymlet@users.noreply.github.com>
2019-08-30 10:02:22 -0700

    Fixed

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class CountUp
+    {
+        for (int i = 0; i < 11; i++)
+			{
+                Console.WriteLine(i);
+	        }
+    }
+}
+
