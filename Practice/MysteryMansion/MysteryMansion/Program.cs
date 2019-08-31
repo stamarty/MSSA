750a396934fa30523af6e855acb60f91abd3adaf
stamarty <7009764+stamarty@users.noreply.github.com>
2019-08-31 16:09:40 -0700

    Thus the adventure begins. A game by Steven and Steven

---
+++ Practice/MysteryMansion/MysteryMansion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TheMysterMansion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mystery Mansion");
+            Console.WriteLine("");
+            Console.Write("What should I call you, stranger? ");
+            string name = Console.ReadLine();
+
+            name = name.ToUpper();
+
+            Console.WriteLine(name +
+                " that's a good name. A good strong name. You'll need strength to get through this mansion, "
+                + name + ", strength, intelligence and not a little bit of luck.");
+
+            Console.ReadLine();
+        }
+    }
+}
