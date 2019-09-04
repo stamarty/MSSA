9dd79f6c543a4fd3c1caa5ed52e5c73da169f789
stamarty <7009764+stamarty@users.noreply.github.com>
2019-09-03 20:55:18 -0700

    working with python

--- Python-LI/Exercise Files/Ch2/conditionals_start.py
+++ Python-LI/Exercise Files/Ch2/conditionals_start.py
@@ -3,12 +3,21 @@
 #
 
 def main():
-  x, y = 10, 100
+  x, y = 1000, 1000
   
   # conditional flow uses if, elif, else  
+  if(x < y):
+    st = "x is less than y"
+  elif(x==y):
+    st = "x is the same as y"
+  else:
+    st = "x is greater than y"
+
+  print(st)
 
   # conditional statements let you use "a if C else b"
   
+  st = "x is less than y" if (x<y) else "x is greater than or the same as y"
 
 if __name__ == "__main__":
   main()
