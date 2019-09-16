17a5679838ad101741d95f4cca277d3d3f5580f4
stamarty <7009764+stamarty@users.noreply.github.com>
2019-09-15 19:32:09 -0700

    Lab4 Module 2 complete WHILE and DO loops

---
+++ IntroCSharpEdEx/Mod2/Lab4Mod2/Lab4Mod2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4Mod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while (n < 6)
+            {
+                Console.WriteLine($"Current value of n is {n}");
+                n++;
+            }
+
+            int x = 0;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (x < 5);
+
+        }
+    }
+}
