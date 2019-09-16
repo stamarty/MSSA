3d8ad0f5eb2d9338266c338a8cf68de51c284a32
stamarty <7009764+stamarty@users.noreply.github.com>
2019-09-15 19:28:46 -0700

    Lab3 module 2 complete. Nested For statement and prime number generator

--- IntroCSharpEdEx/Mod2/Lab3Mod2/Lab3Mod2/Program.cs
+++ IntroCSharpEdEx/Mod2/Lab3Mod2/Lab3Mod2/Program.cs
@@ -10,7 +10,26 @@ namespace Lab3Mod2
     {
         static void Main(string[] args)
         {
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine($"Counter is at {counter}");
+            }
 
+            int outer;
+            int inner;
+
+            for (outer = 2; outer < 100; outer++)
+            {
+                for (inner = 2; inner <= (outer/inner); inner++)
+                {
+                    if ((outer % inner) == 0) break; 
+                }
+
+                if (inner > (outer / inner))
+                {
+                    Console.WriteLine($"{outer} is prime.");
+                }
+            }
         }
     }
 }
