1b730e8fc188b7bb8e31ebf0980e857ec6780cd0
stamarty <7009764+stamarty@users.noreply.github.com>
2019-09-15 17:23:53 -0700

    Finish Lab1 Mod 2, IF statement.

---
+++ IntroCSharpEdEx/Mod2/Lab1Mod2/Lab1Mod2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Mod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter and integer value and press Enter.");
+
+            int input = Int32.Parse(Console.ReadLine());
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("The entered number, {0}, is an even number! ", input);
+            }
+            else
+            {
+                Console.WriteLine("The entered number, {0}, is an odd number! ", input);
+            }
+        }
+    }
+}
