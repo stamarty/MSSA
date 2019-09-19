162ff2fee5f9ad395022969d56ccdaaa307d1340
stamarty <7009764+stamarty@users.noreply.github.com>
2019-09-19 10:40:57 -0700

    CSharpMidterm Begin

---
+++ CSharpMidterm/CSharpMidterm/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMidterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Find 5 functions each from 3 different namespaces. Show examples. Be able to brief. 
+            //TODO Namespace 1
+
+            //TODO Function 1
+            //TODO Function 2
+            //TODO Function 3
+            //TODO Function 4
+            //TODO Function 5
+
+            //TODO Namespace 2
+            //TODO Function 1
+            //TODO Function 2
+            //TODO Function 3
+            //TODO Function 4
+            //TODO Function 5
+
+            //TODO Namespace 3
+            //TODO Function 1
+            //TODO Function 2
+            //TODO Function 3
+            //TODO Function 4
+            //TODO Function 5
+
+
+        }
+    }
+}
