136203da2ef77fe138a05cf243fb7c0f21c3bde2
stamarty <7009764+stamarty@users.noreply.github.com>
2019-09-24 10:50:32 -0700

    namespace 1 half done.

--- CSharpMidterm/CSharpMidterm/Program.cs
+++ CSharpMidterm/CSharpMidterm/Program.cs
@@ -11,10 +11,29 @@ namespace CSharpMidterm
         static void Main(string[] args)
         {
             //Find 5 functions each from 3 different namespaces. Show examples. Be able to brief. 
-            //TODO Namespace 1
+            //TODO System Namespace
 
-            //TODO Function 1
-            //TODO Function 2
+            //TODO Function BEEP
+            //plays a sound through the speakers.
+            Console.Beep();
+            //Console.Beep(500, 2000);
+            //Console.Beep(750, 2000);
+            //Console.Beep(1000, 2000);
+
+            //Console.WriteLine("How many beeps do you want to hear?");
+            //string beepNum = Console.ReadLine();
+            //int x;
+            //Int32.TryParse(beepNum, out x);
+
+            //for (int i = 1; i <= x; i++)
+            //{
+            //    Console.WriteLine("Beep number {0}.", i);
+            //    Console.Beep();
+            //}
+
+            //TODO Function MATH
+            Math.
+
             //TODO Function 3
             //TODO Function 4
             //TODO Function 5
@@ -26,6 +45,7 @@ namespace CSharpMidterm
             //TODO Function 4
             //TODO Function 5
 
+
             //TODO Namespace 3
             //TODO Function 1
             //TODO Function 2
@@ -33,6 +53,8 @@ namespace CSharpMidterm
             //TODO Function 4
             //TODO Function 5
 
+            Console.WriteLine("Hello, World!");
+            Console.Beep();
 
         }
     }
