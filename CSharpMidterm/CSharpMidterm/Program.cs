70d8b2fbae715e94b10efd2bd345773ef64392ef
stamarty <7009764+stamarty@users.noreply.github.com>
2019-09-25 22:08:51 -0700

    Second Namespace 'system.io' completed.

--- CSharpMidterm/CSharpMidterm/Program.cs
+++ CSharpMidterm/CSharpMidterm/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace CSharpMidterm
 {
@@ -10,15 +7,17 @@ namespace CSharpMidterm
     {
         static void Main(string[] args)
         {
-            //Find 5 functions each from 3 different namespaces. Show examples. Be able to brief. 
             //TODO System Namespace
 
             //TODO Function BEEP
             //plays a sound through the speakers.
+
             //Console.Beep();
+
             //Console.Beep(500, 2000);
             //Console.Beep(750, 2000);
             //Console.Beep(1000, 2000);
+            //Console.Beep();
 
             //Console.WriteLine("How many beeps do you want to hear?");
             //string beepNum = Console.ReadLine();
@@ -31,32 +30,49 @@ namespace CSharpMidterm
             //    Console.Beep();
             //}
 
+
+
+            //-------------------------------------
             //TODO Method MATH.POW
             //Provides constants and static methods for trigonometric, logarithmic, and other common mathematical functions.
+            //Hypotenuse - WRONG
 
             //Console.WriteLine("Let's find a hypontenuse! (press 'enter' to continue)");
             //Console.ReadLine();
-
             //Console.WriteLine("What is the length of the short side?");
             //string sLength = Console.ReadLine();
-
             //Console.WriteLine("What is the length of the long side?");
             //string lLength = Console.ReadLine();
-
             //double a, b;
-
             //double.TryParse(sLength, out a);
             //double.TryParse(lLength, out b);
-
             //double aLength = Math.Pow(a, 2);
             //double bLength = Math.Pow(b, 2);
             //double cLength = (aLength + bLength);
-
             //Console.WriteLine("Your hypotenuse is {0}.", cLength);
+
+            //-------------------------------------
+            //TODO - Math.SQRT
+            //Hypotenuse - Correct
+
+            //Console.WriteLine("Let's find a hypontenuse! (press 'enter' to continue)");
+            //Console.ReadLine();
+            //Console.WriteLine("What is the length of the short side?");
+            //string sLength = Console.ReadLine();
+            //Console.WriteLine("What is the length of the long side?");
+            //string lLength = Console.ReadLine();
+            //double a, b;
+            //double.TryParse(sLength, out a);
+            //double.TryParse(lLength, out b);
+            //double aLength = Math.Pow(a, 2);
+            //double bLength = Math.Pow(b, 2);
+            //double cLength = Math.Sqrt(aLength + bLength);
+            //Console.WriteLine("Your hypotenuse is {0}.", cLength);
+
+            //-------------------------------------
 
             //TODO Function Math.Round
             //Rounds a number to an integer by using normal rounding conventions.
-
 
             //double q = Math.Round(31.45);
             //double r = Math.Round(31.55);
@@ -68,18 +84,81 @@ namespace CSharpMidterm
             //Console.WriteLine("31.44555 rounded 2 places is {0}", s);
             //Console.WriteLine("31.44455 rounded 2 places is {0}", t);
 
-
-
-
-            //TODO Function 4
+            //-------------------------------------
             //TODO Function 5
+            //Array type mismatch exception
 
-            //TODO Namespace 2
-            //TODO Function 1
-            //TODO Function 2
-            //TODO Function 3
-            //TODO Function 4
-            //TODO Function 5
+            //string[] siblings = { "john", "jamie", "robert", "simon" };
+            //Object[] objs = (Object[]) siblings;
+            //try
+            //{
+            //    foreach (object name in objs)
+            //    {
+            //        Console.WriteLine(name);
+            //    }
+            //}
+            //catch (ArrayTypeMismatchException)
+            //{
+            //    Console.WriteLine("Exception Thrown");
+            //}
+
+            //try
+            //{
+            //    Object obj = (Object)42;
+            //    objs[3] = obj;
+            //}
+            //catch (ArrayTypeMismatchException)
+            //{
+            //    Console.WriteLine("Array Type Mismatch Exception: Incorrect Type Added");
+            //}
+
+            //-------------------------------------
+            //-------------------------------------
+
+            //TODO System.IO
+            //The System.IO namespace contains types that allow reading and writing to files and data streams, 
+            //and types that provide basic file and directory support.
+
+            //TODO File Class
+            //create method
+
+            //-------------------------------------
+
+            //Console.WriteLine("Let's create a file.");
+            //using (File.Create("E:\\Program Files\\Repos\\MSSA\\CSharpMidterm\\iheartcsharp.txt")) ;
+            //Console.WriteLine("iheartcsharp created.");
+            //Console.ReadLine();
+
+            ////TODO WriteAllText Method
+            //string text = "Hello World";
+            //File.WriteAllText("E:\\Program Files\\Repos\\MSSA\\CSharpMidterm\\iheartcsharp2.txt", text);
+            //Console.WriteLine("iheartcsharp2 created.");
+            //Console.ReadLine();
+
+            ////TODO Copy Method
+
+            //File.Copy("E:\\Program Files\\Repos\\MSSA\\CSharpMidterm\\iheartcsharp2.txt", "E:\\Program Files\\Repos\\MSSA\\CSharpMidterm\\iheartcsharp.txt", true);
+            //Console.WriteLine("iheartcsharp2 copied to iheartcsharp.");
+            //Console.ReadLine();
+
+            ////TODO ReadAllText Method
+
+            //string contents = File.ReadAllText("E:\\Program Files\\Repos\\MSSA\\CSharpMidterm\\iheartcsharp.txt");
+            //Console.WriteLine("iheartcsharp contained the following text: {0} ", contents);
+            //Console.ReadLine();
+
+            ////TODO Delete Method
+            //File.Delete("E:\\Program Files\\Repos\\MSSA\\CSharpMidterm\\iheartcsharp.txt");
+            //Console.WriteLine("iheartcsharp deleted");
+            //Console.ReadLine();
+
+            //File.Delete("E:\\Program Files\\Repos\\MSSA\\CSharpMidterm\\iheartcsharp2.txt");
+            //Console.WriteLine("iheartcsharp2 deleted");
+            //Console.ReadLine();
+
+            //-------------------------------------
+            //-------------------------------------
+
 
 
             //TODO Namespace 3
@@ -90,8 +169,9 @@ namespace CSharpMidterm
             //TODO Function 5
 
             Console.WriteLine("Hello, World!");
-            
+
 
         }
+        
     }
 }
