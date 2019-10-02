131568e9a037c267309b639582bbce452f967dd9
stamarty <7009764+stamarty@users.noreply.github.com>
2019-10-02 15:42:24 -0700

    HanoiTowers complete

---
+++ HanoiTowers/HanoiTowers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanoiTowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hanoi(1, 'A', 'C', 'B');
+        }
+        public static void Hanoi(int n, char from, char to, char intermediary)
+        {
+            if (n > 0)
+            {
+                Hanoi(n - 1, from, intermediary, to);
+                Console.WriteLine(from + "->" + to);
+                Hanoi(n - 1, intermediary, to, from);
+            }
+        }
+    }
+}
