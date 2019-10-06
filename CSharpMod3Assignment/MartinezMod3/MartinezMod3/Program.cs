889a5a7f9e76b0a45c3108d5526b83fb7670e770
stamarty <7009764+stamarty@users.noreply.github.com>
2019-10-06 12:20:38 -0700

    Finish Mod 3 assignment.

---
+++ CSharpMod3Assignment/MartinezMod3/MartinezMod3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections; //required to use the 'Hashtable Collection'
+
+namespace MartinezMod3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new Hashtable collection.
+            Hashtable ingredients = new Hashtable(); //creates a new hashtable this is similar to a dictionary in that it associates keys, with values. 
+            // Add some key/value pairs to the collection.
+            ingredients.Add("Café au Lait", "Coffee, Milk");
+            ingredients.Add("Café Mocha", "Coffee, Milk, Chocolate");
+            ingredients.Add("Cappuccino", "Coffee, Milk, Foam");
+            ingredients.Add("Irish Coffee", "Coffee, Whiskey, Cream, Sugar");
+            ingredients.Add("Macchiato", "Coffee, Milk, Foam");
+            ingredients.Add("Coffee", "Coffee, Coffee, More Coffee"); //ingredients.Add enters data into the hashtable. In this situation only one key for one value.  
+            ingredients.Add("Juice", "No coffee, wish there was coffee, seriously (-;"); //you can use a list as one value for the hashtable. 
+
+            //loop through each key and print associated values.
+            foreach (string key in ingredients.Keys) //call the keys from the hashtable we're iterating through the hashtable here. notice having to cast the data back to a string. 
+                //hashtables are implicitly stored as objects.
+            {
+                Console.WriteLine("The ingredients of a {0} are {1}.",key, ingredients[key]); //prints the key, then the values associated with each key. 
+            }
+
+            Console.WriteLine("----------------");
+
+            foreach (string value in ingredients.Keys) //reversing here and calling values per key first. 
+            {
+                Console.WriteLine("The ingredients {0} are associated with the drink {1}", ingredients[value], value);
+            }
+
+            Console.WriteLine("----------------");
+
+            // Check whether a key exists.
+            if (ingredients.ContainsKey("Café Mocha")) //checking to see if the hashtable contains a specific value. This should be wrapped in a try/catch block.
+            {
+                // Retrieve the value associated with a key.
+                Console.WriteLine("The ingredients of a Café Mocha are: {0}", ingredients["Café Mocha"]); //found "Cafe Mocha" key so print ingredients associated. 
+            }
+        }
+    }
+}
---
+++ CSharpMod3Assignment/MartinezMod3/MartinezMod3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MartinezMod3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MartinezMod3")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("50efbb8b-d88f-4737-aaa7-0361eae31c7e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
