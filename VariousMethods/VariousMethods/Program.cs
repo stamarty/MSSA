8208792301c20a3506d5281d0ba18ea30b9ff68b
stamarty <7009764+stamarty@users.noreply.github.com>
2019-10-16 21:35:07 -0700

    several ways of reversing arrays.

--- VariousMethods/VariousMethods/Program.cs
+++ VariousMethods/VariousMethods/Program.cs
@@ -10,6 +10,48 @@ namespace VariousMethods
     {
         static void Main(string[] args)
         {
+            string[] newArray = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+                "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+            string[] stevenName = new string[6] { "s", "t", "e", "v", "e", "n" };
+
+            ReverseArray(newArray);
+            ReverseArray(stevenName);
+
+            Console.WriteLine("------------");
+
+            string lName = "Martinez";
+            char[] reversedLastName = lName.ToCharArray();
+            Array.Reverse(reversedLastName);
+            foreach (char c in reversedLastName)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine("------------");
+            
+            int[] nums = new int[5] { 1, 2, 3, 4, 5 };
+            Array.Reverse(nums);
+            foreach (int i in nums)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("------------");
+
+        }
+
+        public static string[] ReverseArray(string[] arr)
+        {
+            for (int i = 0, j=arr.Length-1; i < j; i++, j--)
+            {
+                string temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;                
+            }
+
+            Console.WriteLine("[{0}]", string.Join(", ", arr));
+            return arr;
         }
     }
 }
