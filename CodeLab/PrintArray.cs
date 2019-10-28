472fd6d3342a921e0e73fdd915485b4c59a43bca
stamarty <7009764+stamarty@users.noreply.github.com>
2019-10-27 20:20:05 -0700

    Move CodeLab files over - delete  old CL repo

---
+++ CodeLab/PrintArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// <summary>
+//Write the definition of a method PrintArray, which has one parameter, an array of ints. 
+    //The method does not return a value. 
+    //The method prints out each element of the array, on a line by itself, 
+    //in the order the elements appear in the array, and does not print anything else.
+// </summary>
+
+
+public class PrintArray
+{
+    int[] newArray = new int[5] { 1, 2, 3, 4, 5 };
+
+    public static void PrintArray(int[] k)
+        {
+
+            for (int i = 0; i < k.Length; i++)
+            {
+
+                Console.WriteLine(k[i]);
+
+            }
+
+        }
+}
